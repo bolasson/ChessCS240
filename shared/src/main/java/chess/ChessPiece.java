1043f3b5fd8399b556704acb0c2201bbc721c3ba
Bryce Lasson <brycelasson@gmail.com>
2024-05-09 23:37:20 -0600

    All moves except pawn implemented.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,8 @@
 package chess;
 
+import chess.ChessGame.TeamColor;
+
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -52,6 +55,82 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> moves = new ArrayList<>();
+        switch (pieceType) {
+            case KING:
+                moves.addAll(addLinearMoves(board, myPosition, new int[][]{{1,0}, {0,1}, {-1,0}, {0,-1}, {1,1}, {1,-1}, {-1,1}, {-1,-1}},1));
+                break;
+            case QUEEN:
+                moves.addAll(addLinearMoves(board, myPosition, new int[][]{{1,0}, {0,1}, {-1,0}, {0,-1}, {1,1}, {1,-1}, {-1,1}, {-1,-1}},0));
+                break;
+            case BISHOP:
+                moves.addAll(addLinearMoves(board, myPosition, new int[][]{{1,1}, {1,-1}, {-1,1}, {-1,-1}},0));
+                break;
+            case KNIGHT:
+                moves.addAll(addKnightMoves(board, myPosition));
+                break;
+            case ROOK:
+                moves.addAll(addLinearMoves(board, myPosition, new int[][]{{1,0}, {0,1}, {-1,0}, {0,-1}},0));
+                break;
+            case PAWN:
+                // Still need to make the pawn moves.
+                break;
+        }
+        return moves;
     }
+
+    // Helper function for knight
+    private Collection<ChessMove> addKnightMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        int[][] positions = {
+            {2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}
+        };
+    
+        for (int[] position : positions) {
+            int newRow = myPosition.getRow() + position[0];
+            int newCol = myPosition.getColumn() + position[1];
+            if (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
+                ChessPosition newPosition = new ChessPosition(newRow, newCol);
+                ChessPiece pieceAtNewPosition = board.getPiece(newPosition);
+                if (pieceAtNewPosition == null || pieceAtNewPosition.getTeamColor() != this.color) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                }
+            }
+        }
+        return moves;
+    }
+
+    // Helper function for bishop, rook, queen, and king
+    private Collection<ChessMove> addLinearMoves(ChessBoard board, ChessPosition myPosition, int[][] positions, int maxSteps) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+    
+        for (int[] position : positions) {
+            int tempRow = position[0];
+            int tempCol = position[1];
+            int nextRow = row + tempRow;
+            int nextCol = col + tempCol;
+            int steps = 0;
+    
+            while (nextRow >= 0 && nextRow < 8 && nextCol >= 0 && nextCol < 8 && (steps < maxSteps || maxSteps == 0)) {
+                ChessPosition nextPosition = new ChessPosition(nextRow, nextCol);
+                ChessPiece pieceAtNextPosition = board.getPiece(nextPosition);
+                
+                if (pieceAtNextPosition == null) {
+                    moves.add(new ChessMove(myPosition, nextPosition, null));
+                } else {
+                    if (pieceAtNextPosition.getTeamColor() != this.color) {
+                        moves.add(new ChessMove(myPosition, nextPosition, null));
+                    }
+                    break;
+                }
+                nextRow += tempRow;
+                nextCol += tempCol;
+                steps++;
+            }
+        }
+        return moves;
+    }
+    
 }
